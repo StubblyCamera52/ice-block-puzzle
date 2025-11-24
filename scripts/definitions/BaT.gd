a275a11d441d841182d57694c4823c3a0c735f21
Gavan Bess <gavanbess@gmail.com>
2025-11-24 12:19:45 -0800

    blocks and tiles def

---
+++ scripts/definitions/BaT.gd
@@ -0,0 +1,54 @@
+extends Node
+
+#BaT stands for Blocks and Tiles
+
+enum Blocks {
+	Air,
+	Ice,
+	Snow,
+	Player,
+}
+
+enum Tiles {
+	Generic,
+	Ice,
+	Stone,
+	Water
+}
+
+enum SlideBehavior {
+	STOP,
+	DIRECTIONAL,
+	SLIDE,
+}
+
+const TileProperties = {
+	Tiles.Generic: {
+		"walkable": false,
+		"slide_behavior": SlideBehavior.STOP,
+		"slide_direction": Vector2i(0, 0), # 0, 0 means keep going in the direction its already going
+	},
+	Tiles.Stone: {
+		"walkable": true,
+		"slide_behavior": SlideBehavior.STOP,
+		"slide_direction": Vector2i(0, 0)
+	},
+	Tiles.Ice: {
+		"walkable": true,
+		"slide_behavior": SlideBehavior.SLIDE,
+		"slide_direction": Vector2i(0, 0)
+	}
+}
+
+const BlockProperties = {
+	Blocks.Air: {
+		"collision": false,
+		"pushable": false,
+		"max_slide_distance": 0,
+	},
+	Blocks.Ice: {
+		"collision": true,
+		"pushable": true,
+		"max_slide_distance": 3,
+	}
+}
--- scripts/systems/grid_manager.gd
+++ scripts/systems/grid_manager.gd
@@ -1,3 +1,11 @@
 extends Node
 
-var tile_grid: Dictionary[Vector2i, Block]
+var tile_grid: Dictionary[Vector2i, BAT.Tiles]
+var block_grid: Dictionary[Vector2i, BAT.Blocks]
+
+func _ready() -> void:
+	for i in range(14*14):
+		tile_grid.set(Vector2i(i%14, floor(i/14)), BAT.Tiles.Stone)
+		block_grid.set(Vector2i(i%14, floor(i/14)), BAT.Blocks.Air)
+	
+	print(tile_grid, block_grid)
