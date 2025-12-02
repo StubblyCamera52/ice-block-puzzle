7031b4c603e5f55c23f36d4bc07b274ee2671018
Gavan Bess <gavanbess@gmail.com>
2025-12-01 19:55:50 -0800

    update

--- scripts/core/game.gd
+++ scripts/core/game.gd
@@ -5,6 +5,7 @@ var player_facing_direction: Vector2i = Vector2.RIGHT
 
 @onready var UIManager = $UIManager
 @onready var camera: Camera3D = $Camera3D
+@onready var flag: Node3D = $flag
 
 signal level_started()
 signal level_completed()
@@ -13,7 +14,7 @@ signal level_completed()
 func _ready() -> void:
 	GameStateManager.level_completed.connect(on_level_completed)
 	await get_tree().process_frame
-	start_level("test00")
+	start_level("level00")
 	
 
 func _unhandled_key_input(event: InputEvent) -> void:
@@ -62,13 +63,24 @@ func restart_level():
 		GameStateManager.load_level(GameStateManager.current_level)
 
 func start_level(level_id: String):
-	var level = Levels.parse_level_string(Levels.level01)
+	Globals.unlock()
+	Globals.set_input_enabled()
+	var level: LevelData
+	match level_id:
+		"level00": level = Levels.parse_level_string(Levels.level00)
+		"level01": level = Levels.parse_level_string(Levels.level01)
+		"level02": level = Levels.parse_level_string(Levels.level02)
 	if not level: return
-	$Camera3D.position = Vector3((level.grid_size.x/2), 5.0, level.grid_size.y+1)
+	$Camera3D.position = Vector3((level.grid_size.x/2), 5.0, level.grid_size.y+.5)
+	var end_pos = level.goals[0].target_positions[0]
+	flag.position = Vector3(end_pos.x, 0.1, end_pos.y)
 	GameStateManager.load_level(level)
 	level_started.emit()
 
 func on_level_completed(level_data: LevelData):
+	match level_data.level_id:
+		"level00":start_level("level01");return;
+		"level01":start_level("level02");return;
 	Globals.set_input_disabled()
 	Globals.lock()
 	print("complete")
